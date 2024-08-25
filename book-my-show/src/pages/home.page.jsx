d86464c47a819fd955678f9bb37efb6ad058e32f
Sarthak123-glitch <en21cs301692@medicaps.ac.in>
2024-08-25 19:38:23 +0000

    day-2-completed

---
+++ book-my-show/src/pages/home.page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// layout HOC
+import defaultlayout from '../layout/default.layout';
+
+// import Entertainment from '../components/Entertainment/Entertainment.Card';
+// import HeroCarousel from '../components/HeroCarousel/HeroCarousel.component';
+// import PosterSlider from '../components/PosterSlider/PosterSlider.component';
+
+
+
+const Homepage=()=>{
+    return(
+        <div>
+            
+            home</div>
+    )
+}
+
+export default defaultlayout(Homepage);
---
+++ book-my-show/src/pages/movie.page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Movielayout from '../layout/Movie.layout';
+
+
+const moviepage = () => {
+  return (
+    <div>moviepage</div>
+  )
+}
+
+export default Movielayout(moviepage)
